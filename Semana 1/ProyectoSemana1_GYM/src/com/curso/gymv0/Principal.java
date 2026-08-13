d701d6de82a22185f04522201d6980829421772d
saulhdz <saul.hernandez.22isc@tecsanpedro.edu.mx>
2026-08-13 17:26:30 -0600

    Prueba version 0.1

---
+++ Semana 1/ProyectoSemana1_GYM/src/com/curso/gymv0/Principal.java
@@ -0,0 +1,11 @@
+package com.curso.gymv0;
+
+public class Principal {
+
+	public static void main(String[] args) {
+System.out.println("----GYM SMARTFIT----");
+System.out.println("     Bienvenid@     ");
+
+	}
+
+}
