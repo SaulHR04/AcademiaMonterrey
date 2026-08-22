0467f4e6f1cc8f4eb63928d754a4cb5e1f7e5909
Saul <SaulHR24@hotmail.com>
2026-08-21 19:56:17 -0600

    Conexion Mongo-Proyecto

--- Semana 2/16-spring-boot-rest-crud-youtubers-mongodb/src/main/java/com/luv2code/springboot/cruddemo/entity/Youtubers.java
+++ Semana 2/16-spring-boot-rest-crud-youtubers-mongodb/src/main/java/com/luv2code/springboot/cruddemo/entity/Youtubers.java
@@ -3,78 +3,91 @@ package com.luv2code.springboot.cruddemo.entity;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "employees")
+@Document(collection = "Youtubers")
 public class Youtubers {
 
-    // define fields
-    //
-    // El id es String, no int: MongoDB genera un ObjectId (hexadecimal de 24
-    // caracteres). No lleva @GeneratedValue — el driver lo asigna al insertar.
+   
     @Id
     private String id;
-
-    // Sin @Column: MongoDB no tiene esquema, así que el campo del documento se
-    // llama igual que la propiedad Java. No hay nada que mapear.
-    private String firstName;
-
-    private String lastName;
-
-    private String email;
-
-
-    // define constructors
+    private String nombre;
+    private String tipo_contenido;
+    private String seguidores;
+    private int videos_totales;
+    private String equipo_de_trabajo;
+    private int funas; 
+    private String tiempo_youtube;
+    private boolean monetizacion;
+	
     public Youtubers() {
-
+    }
+    
+    public Youtubers(String id, String nombre, String tipo_contenido, String seguidores, int videos_totales,
+			String equipo_de_trabajo, int funas, String tiempo_youtube, boolean monetizacion) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.tipo_contenido = tipo_contenido;
+		this.seguidores = seguidores;
+		this.videos_totales = videos_totales;
+		this.equipo_de_trabajo = equipo_de_trabajo;
+		this.funas = funas;
+		this.tiempo_youtube = tiempo_youtube;
+		this.monetizacion = monetizacion;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getTipo_contenido() {
+		return tipo_contenido;
+	}
+	public void setTipo_contenido(String tipo_contenido) {
+		this.tipo_contenido = tipo_contenido;
+	}
+	public String getSeguidores() {
+		return seguidores;
+	}
+	public void setSeguidores(String seguidores) {
+		this.seguidores = seguidores;
+	}
+	public int getVideos_totales() {
+		return videos_totales;
+	}
+	public void setVideos_totales(int videos_totales) {
+		this.videos_totales = videos_totales;
+	}
+	public String getEquipo_de_trabajo() {
+		return equipo_de_trabajo;
+	}
+	public void setEquipo_de_trabajo(String equipo_de_trabajo) {
+		this.equipo_de_trabajo = equipo_de_trabajo;
+	}
+	public int getFunas() {
+		return funas;
+	}
+	public void setFunas(int funas) {
+		this.funas = funas;
+	}
+	public String getTiempo_youtube() {
+		return tiempo_youtube;
+	}
+	public void setTiempo_youtube(String tiempo_youtube) {
+		this.tiempo_youtube = tiempo_youtube;
+	}
+	public boolean isMonetizacion() {
+		return monetizacion;
+	}
+	public void setMonetizacion(boolean monetizacion) {
+		this.monetizacion = monetizacion;
+	}
+    
     }
 
-    public Youtubers(String firstName, String lastName, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-
-    // define getter/setter
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    // define toString
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id='" + id + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
-}
